011eeb815f84d2e0afdf65fc6baba55c854ad8f3
Zane Rockenbaugh <zane@liquid-labs.com>
2024-02-04 23:46:41 -0600

    isolated react processing to jsx files; enabled globals

--- src/eslint.config.cjs
+++ src/eslint.config.cjs
@@ -1,54 +1,51 @@
-/*
-import { join } from 'node:path'
-
-import babelParser from '@babel/eslint-parser'
-import importPlugin from 'eslint-plugin-import'
-import react from 'eslint-plugin-react'
-import js from '@eslint/js'
-*/
+const { readFileSync } = require('node:fs')
 const { join } = require('node:path')
 
 const babelParser = require('@babel/eslint-parser')
+const globalsPkg = require('globals')
 const importPlugin = require('eslint-plugin-import')
 const react = require('eslint-plugin-react')
 const js = require('@eslint/js')
 
+const globals = {}
+const packageContents = readFileSync('./package.json', { encoding : 'utf8' })
+const packageJSON = JSON.parse(packageContents)
+const globalKeys = packageJSON._sdlc?.globals || [ 'node' ]
+for (const key of globalKeys) {
+  Object.assign(globals, globalsPkg[key])
+}
+
 const eslintConfig = [
   {
-    files: ['**/*.js', '*/*.mjs', '**/*.cjs', '**/*.jsx'],
-    languageOptions: {
-      parser: babelParser,
+    files           : ['**/*.js', '*/*.mjs', '**/*.cjs', '**/*.jsx'],
+    languageOptions : {
+      parser        : babelParser,
       parserOptions : {
         sourceType        : 'module',
         requireConfigFile : true,
         babelOptions      : {
           configFile : join(__dirname, 'babel', 'babel.config.cjs')
         },
-        ecmaFeatures: {
-          jsx: true
-        },
       },
-      ecmaVersion: 6
+      ecmaVersion : 'latest',
+      globals,
     },
-    plugins: { 
-      import: importPlugin,
-      react
+    plugins : { 
+      import : importPlugin,
     },
-    rules : Object.assign({
-        'import/export'     : 'off', // TODO: this rule is incompatible with 'export * from 'XXX'
-        'import/extensions' : ['error', 'never', { json : ['error', 'always'] }],
-      },
-      react.configs.recommended.rules
-    )
+    rules : {
+      'import/export'     : 'off', // TODO: this rule is incompatible with 'export * from 'XXX'
+      'import/extensions' : ['error', 'never', { json : ['error', 'always'] }],
+    }
   },
   js.configs.recommended,
   // importPlugin.configs.recommended,
   // react.configs.recommended, // see Object.assign above
   {
-    rules   : {
-      'brace-style'       : [2, 'stroustrup', { allowSingleLine : true }],
-      curly               : [2, 'multi-line'],
-      indent              : [2, 2, {
+    rules : {
+      'brace-style' : [2, 'stroustrup', { allowSingleLine : true }],
+      curly         : [2, 'multi-line'],
+      indent        : [2, 2, {
         FunctionDeclaration : { body : 1, parameters : 2 },
         ignoredNodes        : ['JSXElement', 'JSXElement > *', 'JSXAttribute', 'JSXIdentifier', 'JSXNamespacedName', 'JSXMemberExpression', 'JSXSpreadAttribute', 'JSXExpressionContainer', 'JSXOpeningElement', 'JSXClosingElement', 'JSXText', 'JSXEmptyExpression', 'JSXSpreadChild']
       }],
@@ -69,17 +66,45 @@ const eslintConfig = [
       'prefer-spread'               : 2,
       semi                          : ['error', 'never'],
       'space-before-function-paren' : [2, 'never'],
-      'array-callback-return'   : 2,
-      'guard-for-in'            : 2,
-      'no-caller'               : 2,
-      'no-extra-bind'           : 2,
-      'no-multi-spaces'         : 2,
-      'no-new-wrappers'         : 2,
-      'no-throw-literal'        : 2,
-      'no-unexpected-multiline' : 2,
-      'no-with'                 : 2,
-      'prefer-regex-literals'   : 'error',
-      yoda                      : 2
+      'array-callback-return'       : 2,
+      'guard-for-in'                : 2,
+      'no-caller'                   : 2,
+      'no-extra-bind'               : 2,
+      'no-multi-spaces'             : 2,
+      'no-new-wrappers'             : 2,
+      'no-throw-literal'            : 2,
+      'no-unexpected-multiline'     : 2,
+      'no-with'                     : 2,
+      'prefer-regex-literals'       : 'error',
+      yoda                          : 2
+    }
+  },
+  {
+    files           : ["**/*.jsx"],
+    languageOptions : {
+      parserOptions : {
+        ecmaFeatures : {
+          jsx : true
+        },
+      },
+      plugins  : { react },
+      settings : {
+        react : {
+          version : 'detect'
+        }
+      },
+      rules : {
+        // can't use 'react.config.recommended directly because as of writing it's still usin the old eslint.rc style
+        ...react.configs.recommended.rules
+      }
+    }
+  },
+  {
+    files           : ["**/_tests_/**", "**/*.test.{cjs,js,jsx,mjs}"],
+    languageOptions : {
+      globals : {
+        ...globalsPkg.jest
+      }
     }
   }
 ]
--- src/test/eslint.config.test.cjs
+++ src/test/eslint.config.test.cjs
@@ -1,18 +1,18 @@
-/* global describe expect test */
 const { resolve } = require('node:path')
 const { FlatESLint } = require('eslint/use-at-your-own-risk')
 
-const config = require('../../dist/eslint.config')
+// erroneously comaplains that '.config' is the extension
+const config = require('../../dist/eslint.config') /* eslint-disable-line import/extensions */
 
 describe('eslint.config.cjs', () => {
   test('detects non-literal regex', async() => {
     const eslint = new FlatESLint({
-      overrideConfig: config,
+      overrideConfig     : config,
       overrideConfigFile : resolve(__dirname, '..', '..', 'dist', 'eslint.config.cjs')
     })
 
     const results = await eslint.lintFiles('src/test/data/non-literal-regex/**/*')
-
+    
     expect(results).toHaveLength(1)
     expect(results[0].errorCount).toBe(1)
     expect(results[0].messages).toHaveLength(1)
