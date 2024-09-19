643b62e0455cb1ba311c2b3a7a618efd40a4be83
Zane Rockenbaugh <zane@liquid-labs.com>
2024-09-19 14:35:28 -0500

    rework to make mock readFile results more configurable

--- src/cli/lib/test/mock-read-file.mjs
+++ src/cli/lib/test/mock-read-file.mjs
@@ -1,11 +1,19 @@
 import { jest } from '@jest/globals'
 
+const readFile = jest.fn()
+
 jest.unstable_mockModule('node:fs/promises', () => ({
-  readFile: async () =>
-`src2/*.mjs
+  readFile
+}))
+
+const setReadFileResults = (results) => 
+  readFile.mockImplementation(async () => results)
+
+setReadFileResults(`src2/*.mjs
 # foo
   
 src/**/*.mjs
 
-`
-}))
+`)
+
+export { setReadFileResults }
