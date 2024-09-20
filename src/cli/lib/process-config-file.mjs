65de36f1f54b18360d7d8e483d160244c5078289
Zane Rockenbaugh <zane@liquid-labs.com>
2024-09-20 13:48:39 -0500

    test and fix processConfigFile

--- src/cli/lib/process-config-file.mjs
+++ src/cli/lib/process-config-file.mjs
@@ -5,13 +5,13 @@ import yaml from 'js-yaml'
 const processConfigFile = async (path) => {
   const lowerPath = path.toLowerCase()
   if (lowerPath.endsWith('.json') || lowerPath.endsWith('.yml') || lowerPath.endsWith('.yaml')) {
-    const contents = readFile(path, { encoding: 'utf8' })
+    const contents = await readFile(path, { encoding: 'utf8' })
     const json = yaml.load(contents)
     return json
   }
   else {
     const config = await import(path)
-    return config
+    return config.default
   }
 }
 
---
+++ src/cli/lib/test/data/config.js
@@ -0,0 +1,6 @@
+// because we start with ESM, this is considered ESM
+const config = {
+  foo: 1
+}
+
+export default config
